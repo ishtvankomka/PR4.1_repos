c133b072fa0792bbc026bf4fabb843816b9d837a
ishtvankomka <you@example.com>
2021-10-05 14:24:46 +0300

    first master commit

---
+++ PR4.1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int k, N, i;
+	double S;
+
+	cout << "N = "; cin >> N;
+
+	S = 0;
+	k = 1;
+	while (k <= N)
+	{
+		S += (1 + sqrt(1 + (k * k))) / (k * k);
+		k++;
+	}
+	cout << S << endl;
+	
+	S = 0;
+	k = 1;
+	do {
+		S += (1 + sqrt(1 + (k * k))) / (k * k);
+		k++;
+	} while (k <= N);
+	cout << S << endl;
+
+	
+	S = 0;
+	for (k = 1; k <= N; k++)
+	{
+		S += (1 + sqrt(1 + (k * k))) / (k * k);
+	}
+	cout << S << endl;
+
+	
+	S = 0;
+	for (k = N; k >= 1; k--)
+	{
+		S += (1 + sqrt(1 + (k * k))) / (k * k);
+	}
+	cout << S << endl;
+	
+	return 0;
+}
